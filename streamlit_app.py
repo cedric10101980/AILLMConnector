e83ecae873bbaf76a3c4cf823cfd800461df3274
Cedric Simon DSouza <122440221+cedric10101980@users.noreply.github.com>
2024-11-22 13:10:38 +0530

    Update streamlit_app.py
    Added a new model

--- streamlit_app.py
+++ streamlit_app.py
@@ -191,6 +191,7 @@ def createCampaignIdMaps():
 def createSideBar():
     with st.sidebar:
         models = {
+            "o1-preview": "🤖",
             "gpt-4o-mini": "🌱",
             "gpt-4-0125-preview": "🤖",
             "gpt-4-turbo-preview": "🚀",
